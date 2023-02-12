0ef4efaf08d98b2855a15778fecd20f4502b96f6
DaviBataglia20 <davi.bataglia14@gmail.com>
2023-02-12 10:59:18 +0100

    import app css borrada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
+
 
 import HomePage from "./components/HomePage/HomePage";
 import MapPage from "./components/MapPage/MapPage";
