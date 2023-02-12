a08298bc58c029258b3e35ec3646261b5eba266b
DaviBataglia20 <davi.bataglia14@gmail.com>
2023-02-12 09:51:22 +0100

    Main Router y Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+
+import HomePage from "./components/HomePage/HomePage";
+import MapPage from "./components/MapPage/MapPage";
+import LandingsPage from "./components/LandingsPage/LandingsPage";
+import DetailsPage from "./components/DetailsPage/DetailsPage";
+import FormPage from "./components/FormPage/FormPage";
+import FavouritesPage from "./components/FavouritesPage/FavouritesPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}>
+          <Route path="/landings" element={<MapPage />} />
+          <Route path="/landings/list" element={<LandingsPage />} />
+          <Route path="/landings/details/:id" element={<DetailsPage />} />
+          <Route path="/landings/create" element={<FormPage />} />
+          <Route path="/landings/update/:id" element={<FormPage />} />
+          <Route path="/favourites" element={<FavouritesPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
