ebeb9ee47056d33d72f2828f5d4f7cf4cc3fedc1
ceci <ceciliasegura.t@hotmail.com>
2023-02-13 17:38:31 +0100

    change font size

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,7 +2,7 @@
 export default function Logo(){
     return(
         <div className="div-logo">
-            <h1 className="font-monoton"> NASA <span className="font-dancing text-fuchsia-500"> api</span></h1>
+            <h1 className="font-monoton font-thin text-3xl"> NASA <span className="font-dancing text-fuchsia-500"> api</span></h1>
             
         </div>
     )
