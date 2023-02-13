9f774b55cd7f56ac4ac0028139b98ee80f0a4651
ceci <ceciliasegura.t@hotmail.com>
2023-02-13 23:27:40 +0100

    feature footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+export default function Footer() {
+    return (
+        <>
+            <div className= "flex gap-14 justify-center items-center p-8">
+                <div>
+                    <h1 className="decoration-fuchsia-50">Contact</h1>
+                    <p>nasa.api@nasa-api.com</p>
+                    <p>Work whith us.</p>
+                </div>
+
+
+                <div>
+                    <h2>About</h2>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem ipsum dolor sit.</p>
+                    <p>Lorem ipsum dolor.</p>
+
+                </div>
+
+                <div></div>
+
+                <div>
+                    <h3>Documentation</h3>
+                    <p>API documentation.</p>
+
+                </div>
+            </div>
+        </>
+    )
+}
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,13 @@
 
-
-
-
+// import Logo from "../Logo/Logo"
+import Footer from "../Footer/Footer"
 
 export default function HomePage() {
-   
+    return (
+        <>
+            {/* <Logo /> */}
+            <Footer />
+        </>
+    )
+
 }
