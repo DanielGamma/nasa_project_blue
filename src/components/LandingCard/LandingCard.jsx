c220bdf398947b34e51ebcf932156569503b6838
millard2santos <millard2santos@gmail.com>
2023-02-13 17:54:52 +0100

    LandingCard-NoHoverYet

--- src/components/LandingCard/LandingCard.jsx
+++ src/components/LandingCard/LandingCard.jsx
@@ -1,14 +1,20 @@
 
-const LandingsCard = ({asteroid}) => {
 
+
+const LandingsCard = ({ asteroid }) => {
+    console.log(asteroid)
 
 
     return (
         <div className="text-white bg-purple-900 flex justify-center items-center w-60 h-60 font-semibold text-2xl text-center rounded-lg">
-           
-            <p>{asteroid.name}</p>
+            <p>{Object.hasOwn(asteroid, 'name') ? asteroid.name : ''}</p>
         </div>
     )
 }
 
 export default LandingsCard
+
+
+    // < p > Masa: { Object.hasOwn(asteroid, 'mass') ? asteroid.mass : '' }</p > * /
+    //     < p > Latitud: { Object.hasOwn(asteroid, 'geolocation') ? asteroid.geolocation.latitude : '' }</p >
+    //         <p>Longitud: {Object.hasOwn(asteroid, 'geolocation') ? asteroid.geolocation.longitude : ''}</p>
