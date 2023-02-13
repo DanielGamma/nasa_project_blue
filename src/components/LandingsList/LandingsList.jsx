569decf2faa7401abcfcdc352bd4276abe9fc0a4
millard2santos <millard2santos@gmail.com>
2023-02-13 17:23:23 +0100

    LandingList

--- src/components/LandingsList/LandingsList.jsx
+++ src/components/LandingsList/LandingsList.jsx
@@ -1,5 +1,20 @@
-const LandingsList = ({asteroids}) => {
+import LandingCard from "../LandingCard/LandingCard"
 
+
+
+
+const LandingsList = ({ asteroids }) => {
+
+   
+
+    return (
+        <div className="flex gap-4 flex-wrap">
+            
+            {
+                asteroids.map((asteroid, i) => <LandingCard key={i} asteroid={asteroid} />)
+            }
+        </div>
+    )
 }
 
 export default LandingsList
