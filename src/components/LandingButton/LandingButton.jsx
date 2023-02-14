07608759004d9de840011d4fdd67a342050229c1
millard2santos <millard2santos@gmail.com>
2023-02-14 12:18:01 +0100

    log fixed

---
+++ src/components/LandingButton/LandingButton.jsx
@@ -0,0 +1,12 @@
+
+
+const LandingButtons = () => {
+
+
+    return (
+        <>
+            <i className="text-green-600 fa-solid fa-pen-to-square"></i>
+            <i className="text-red fa-solid fa-trash-can"></i>
+        </>
+    )
+}
