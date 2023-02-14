071717420ec10883d51d6b5fb4fcf18b15d13cff
DaviBataglia20 <davi.bataglia14@gmail.com>
2023-02-14 11:36:40 +0100

    solucionadoNavLink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import Logo from "../sub-components/Logo/Logo";
-import NavButton from "../sub-components/NavButtons/NavButton";
+import NavLink from "../sub-components/NavLink/NavLink";
+
 export default function Header (){
     return(
         <header className=" w-full box-border flex justify-between items-center px-7 bg-[#2A1C3D]">
             <Logo/>
-            <NavButton/>
+            <nav className="flex justify-between gap-x-5 pr-4">
+                <NavLink texto={"Home"} path={"/"}/>       
+                <NavLink texto={"Map"} path={"landings"}/>       
+                <NavLink texto={"Landings"} path={"landings/list"}/>       
+                <NavLink texto={"Favourites"} path={"favourites"}/>       
+            </nav>
         </header>
     )
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,12 @@
 import Header from "../../components/Header/Header"
-
+import Footer from "../../components/Footer/Footer"
 export default function HomePage() {
     return(
+        <>
         <Header/>
+        <div className="h-[100vh]"></div>
+        <Footer />
+        </>
     )
    
 }
