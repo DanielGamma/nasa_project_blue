8766b3064261cc426342f731d96c70c389784b3d
millard2santos <millard2santos@gmail.com>
2023-02-14 15:17:18 +0100

    MapPage to fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import Logo from "../sub-components/Logo/Logo";
 import NavButton from "../sub-components/NavButtons/NavButton";
 export default function Header (){
     return(
-        <header className=" w-full box-border flex justify-between items-center px-7 bg-[#2A1C3D]">
+        <header className=" w-full flex justify-between items-center px-7 bg-[#2A1C3D]">
             <Logo/>
             <NavButton/>
         </header>
