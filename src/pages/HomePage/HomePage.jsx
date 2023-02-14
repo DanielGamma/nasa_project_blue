f35e93cee453aa2964827f6fe4423a924881e66f
ceci <ceciliasegura.t@hotmail.com>
2023-02-14 12:13:49 +0100

    Add colors to tailwind config and fix dirs structure

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,12 @@
+import Footer from "../../components/Footer/Footer"
 import Header from "../../components/Header/Header"
 
 export default function HomePage() {
-    return(
-        <Header/>
+    return (
+        <>
+            <Header />
+            <Footer />
+        </>
     )
-   
+
 }
