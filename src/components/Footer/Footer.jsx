4148fa3d459d78b686f15d580487636f8838519d
ceci <ceciliasegura.t@hotmail.com>
2023-02-14 11:15:53 +0100

    feature footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,28 @@
 export default function Footer() {
     return (
         <>
-            <div className="flex gap-14 justify-center items-center p-8">
-                <div>
+            <div className="flex gap-14 justify-center items-start p-8 w-full bg-[#2A1C3D]">
+                <div className="">
                     <p className="text-fuchsia-500 text-2xl p-4 mt-1.5 font-semibold">Contact</p>
-                    <p className="font-medium">nasa.api@nasa-api.com</p>
-                    <p className="font-medium">Work whith us.</p>
+                    <p className="font-medium text-white">nasa.api@nasa-api.com</p>
+                    <p className="font-medium text-white">Work whith us.</p>
                 </div>
 
-                <div className="text-fuchsia-500 h-8 w-1" ></div>
-
-                <div>
+                
+                <div className=" bg-fuchsia-500 h-[150px] w-0.5" ><p></p></div>
+                <div className="">
                     <p className="text-fuchsia-500 text-2xl p-4 mt-1.5 font-semibold">About</p>
-                    <p className="font-medium">Lorem ipsum dolor sit amet.</p>
-                    <p className="font-medium">Lorem ipsum dolor sit.</p>
-                    <p className="font-medium">Lorem ipsum dolor.</p>
+                    <p className="font-medium text-white">Lorem ipsum dolor sit amet.</p>
+                    <p className="font-medium text-white">Lorem ipsum dolor sit.</p>
+                    <p className="font-medium text-white">Lorem ipsum dolor.</p>
 
                 </div>
 
-                <div className="text-fuchsia-500 h-8 w-1" ></div>
+                <div className=" bg-fuchsia-500 h-[150px] w-0.5" ><p></p></div>
 
-                <div>
+                <div className="">
                     <p className="text-fuchsia-500 text-2xl p-4 mt-1.5 font-semibold">Documentation</p>
-                    <p className="font-medium">API documentation.</p>
+                    <p className="font-medium text-white">API documentation.</p>
 
                 </div>
             </div>
