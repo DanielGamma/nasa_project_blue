63aa4844a84eadf7e393c870ec12a3f12ae076b0
millard2santos <millard2santos@gmail.com>
2023-02-14 18:02:16 +0100

    LandingCard Ongoing

--- src/components/LandingCard/LandingCard.jsx
+++ src/components/LandingCard/LandingCard.jsx
@@ -1,13 +1,23 @@
 
-
+import './LandingCard.css'
 
 const LandingsCard = ({ asteroid }) => {
-    
 
+    console.log(asteroid)
 
     return (
-        <div className="text-white bg-violin-dark flex justify-center items-center w-60 h-60 font-semibold text-2xl text-center rounded-lg">
+        <div className="card group text-white bg-violin-dark rounded-lg
+        flex flex-col justify-center items-center
+        w-60 h-60 p-4 
+        font-semibold text-2xl text-center
+        ">
             <p>{Object.hasOwn(asteroid, 'name') ? asteroid.name : ''}</p>
+            <div className='hover hidden group-hover:flex  '>
+                <p>{asteroid.mass}</p>
+                <p>Latitud: { Object.hasOwn(asteroid, 'geolocation') ? asteroid.geolocation.latitude : '' }</p>
+                <p>Longitud: {Object.hasOwn(asteroid, 'geolocation') ? asteroid.geolocation.longitude : ''}</p>
+
+            </div>
         </div>
     )
 }
