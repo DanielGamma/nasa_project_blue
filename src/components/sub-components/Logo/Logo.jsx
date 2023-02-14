b288ba3046184facaccad82fa3011b2d5818de6f
DaviBataglia20 <davi.bataglia14@gmail.com>
2023-02-14 10:59:37 +0100

    header

---
+++ src/components/sub-components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+
+export default function Logo(){
+    return(
+        <div className="p-3">
+            <h1 className="font-monoton font-thin text-6xl text-white"> NASA <span className="font-dancing text-fuchsia-500"> api</span></h1>
+        </div>
+    )
+}
