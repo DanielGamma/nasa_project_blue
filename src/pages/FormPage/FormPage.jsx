89440fbcc20cd66976055b143c6a32f83992c998
ceci <ceciliasegura.t@hotmail.com>
2023-02-15 19:32:42 +0100

    feature create_form

--- src/pages/FormPage/FormPage.jsx
+++ src/pages/FormPage/FormPage.jsx
@@ -1,6 +1,15 @@
+import Header from "../../components/Header/Header.jsx"
+import Footer from "../../components/Footer/Footer.jsx"
 import Form from "../../components/Form/Form.jsx"
-export default function FormPage (){
-    return(
-        <Form/>
+export default function FormPage() {
+
+
+    return (
+        <>
+            <Header />
+            <Form />
+            <Footer />
+
+        </>
     )
 }
