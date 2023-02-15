53444b4fd57645e75575e0bf74be566a6462ec9f
millard2santos <millard2santos@gmail.com>
2023-02-15 13:29:25 +0100

    ButtonsLeft

--- src/components/LandingCard/LandingCard.jsx
+++ src/components/LandingCard/LandingCard.jsx
@@ -6,17 +6,23 @@ const LandingsCard = ({ asteroid }) => {
     console.log(asteroid)
 
     return (
-        <div className="card group text-white bg-violin-dark rounded-lg
-        flex flex-col justify-center items-center
-        w-60 h-60 p-4 
+        <div className="group text-white bg-violin-dark rounded-lg
+        flex flex-col justify-center items-center relative overflow-hidden
+        w-60 h-60 
         font-semibold text-2xl text-center
-        ">
-            <p>{Object.hasOwn(asteroid, 'name') ? asteroid.name : ''}</p>
-            <div className='hover hidden group-hover:flex  '>
-                <p>{asteroid.mass}</p>
-                <p>Latitud: { Object.hasOwn(asteroid, 'geolocation') ? asteroid.geolocation.latitude : '' }</p>
-                <p>Longitud: {Object.hasOwn(asteroid, 'geolocation') ? asteroid.geolocation.longitude : ''}</p>
+        "><div className='font-normal relative
+                            translate-y-10 flex-col justify-center items-center
+                        group-hover:-translate-y-10
+                         transition-all duration-700'>
+                <p className='card inset-0 group-hover:text-xl transition-all duration-700 font-semibold text-3xl text-center'>{Object.hasOwn(asteroid, 'name') ? asteroid.name : ''}</p>
+                <div className='hover mt-5 relative opacity-0
+                                
+                                 group-hover:opacity-100 transition-all duration-700 delay-300'>
+                    <p className='text-lg text-left'>Mass: {asteroid.mass}</p>
+                    <p className='text-lg text-left'>Latitud: {Object.hasOwn(asteroid, 'geolocation') ? asteroid.geolocation.latitude : ''}</p>
+                    <p className='text-lg text-left'>Longitud: {Object.hasOwn(asteroid, 'geolocation') ? asteroid.geolocation.longitude : ''}</p>
 
+                </div>
             </div>
         </div>
     )
