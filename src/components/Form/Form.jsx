5466e6e9ccb194d1f4b47c89f1fe9905bb6a7e0b
ceci <ceciliasegura.t@hotmail.com>
2023-02-15 16:43:02 +0100

    add tailwind

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,19 +1,23 @@
 export default function Form() {
     return (
-        <>
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" />
-            <label htmlFor="id">Id</label>
-            <input type="text" id="id" />
-            <label htmlFor="class">Class</label>
-            <input type="text" id="class" />
-            <label htmlFor="mass">Mass</label>
-            <input type="text" id="mass" />
-            <label htmlFor="latitud">Latitud</label>
-            <input type="text" id="latitud" />
-            <label htmlFor="longitud">Longitud</label>
-            <input type="text" id="longitud" />
-            <button type="submit"></button>
-        </>
+        <div className="bg-indigo-700">
+            <form className="flex flex-col justify-center  items-center pt-8">
+                <label className="text-white" htmlFor="name">Name:</label>
+                <input className="rounded-full border-2 border-fuchsia-500" type="text" id="name" />
+                <label className="text-white" htmlFor="id">Id:</label>
+                <input className="rounded-full border-2 border-fuchsia-500" type="text" id="id" />
+                <label className="text-white" htmlFor="class">Class:</label>
+                <input className="rounded-full border-2 border-fuchsia-500" type="text" id="class" />
+                <label className="text-white" htmlFor="mass">Mass:</label>
+                <input className="rounded-full border-2 border-fuchsia-500" type="text" id="mass" />
+                <label className="text-white" htmlFor="latitud">Latitud:</label>
+                <input className="rounded-full border-2 border-fuchsia-500" type="text" id="latitud" />
+                <label className="text-white" htmlFor="longitud">Longitud:</label>
+                <input className="rounded-full border-2 border-fuchsia-500" type="text" id="longitud" />
+                <button type="submit"></button>
+            </form>
+        </div>
+
     )
 }
+
