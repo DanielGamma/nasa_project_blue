774f480476107080f454b2b64dd17f1fab0037de
DanielGamma <daniel.carrasco@gammatech.school>
2023-02-17 09:47:19 +0100

    Solved Davi's shit

--- src/components/FavButton/FavButton.jsx
+++ src/components/FavButton/FavButton.jsx
@@ -2,7 +2,7 @@ export default function FavButton({fromDetail,asteroid,setFavAstro}){
     const fav = () => {
         
         setFavAstro(prev => {
-            prev === false?  [asteroid.id] : [...prev,asteroid.id]
+            return prev === false?  [asteroid.id] : [...prev,asteroid.id]
     })
     return(
         <i onClick={fav} className={`fa-regular fa-heart cursor-pointer ${fromDetail ? 'text-4xl' : ''} hover:scale-110 transition-transform duration-300`}></i>
